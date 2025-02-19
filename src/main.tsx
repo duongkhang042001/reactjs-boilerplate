de127f6e342fea83cd9fd7118527f12dcd7afbfc
Khang Duong <khangdt10@fpt.com>
2025-02-19 17:01:30 +0700

    refactor: remove StrictMode wrapper from main application render

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.tsx'
@@ -6,9 +5,7 @@ import { Provider } from 'react-redux'
 import Store from './stores/index.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={Store}>
-      <App />
-    </Provider>
-  </StrictMode>,
+  <Provider store={Store}>
+    <App />
+  </Provider>
 )
